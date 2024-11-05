443d481c67fd8f06f6ea58da251a8c857422bf62
David Lee <Dai180293@gmail.com>
2024-11-05 05:23:29 +0000

    adjusted the header to use a mobile bottom nav layout on mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,8 @@ import { FaDiscord, FaLinkedinIn, FaGithubAlt } from "react-icons/fa";
 const Header = () => {
     const path = usePathname();
     return (
-        <header className="sticky top-0 bg-gradient-to-b from-background from-40% p-5 sm:p-7 md:p-10 backdrop-blur-[2px]">
-            <div className="customWidth flex justify-end sm:justify-between items-center">
+        <header className="w-2/3 max-w-80 sm:max-w-none sm:w-full fixed bottom-2 left-1/2 -translate-x-1/2 sm:sticky sm:top-0 sm:left-0 sm:translate-x-0 bg-foreground sm:bg-transparent sm:bg-gradient-to-b from-background from-40% px-5 py-3 sm:p-7 backdrop-blur-[2px] rounded-full sm:rounded-none">
+            <div className="customWidth flex justify-center sm:justify-between items-center">
                 <ul className="hidden sm:flex space-x-4">
                     <li className="relative border border-foreground25 rounded-md overflow-hidden group">
                         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-50 size-0 bg-foreground group-hover:size-full pointer-events-none transition-all duration-300 rounded-md"></div>
@@ -40,14 +40,14 @@ const Header = () => {
                         </Link>
                     </li>
                 </ul>
-                <ul className="flex space-x-4">
+                <ul className="w-full justify-between sm:w-fit flex space-x-4">
                     <li>
                         <Link
                             href="/"
                             className={
                                 path === "/"
-                                    ? "text-foreground"
-                                    : "text-foreground50 hover:text-foreground"
+                                    ? "text-background font-bold sm:font-normal sm:text-foreground"
+                                    : "text-background50 sm:text-foreground50 hover:text-foreground"
                             }
                         >
                             index
@@ -58,8 +58,8 @@ const Header = () => {
                             href="/resume"
                             className={
                                 path === "/resume"
-                                    ? "text-foreground"
-                                    : "text-foreground50 hover:text-foreground"
+                                    ? "text-background font-bold sm:font-normal sm:text-foreground"
+                                    : "text-background sm:text-foreground50 sm:hover:text-foreground"
                             }
                         >
                             resume
@@ -70,8 +70,8 @@ const Header = () => {
                             href="/contact"
                             className={
                                 path === "/contact"
-                                    ? "text-foreground"
-                                    : "text-foreground50 hover:text-foreground"
+                                    ? "text-background font-bold sm:font-normal sm:text-foreground"
+                                    : "text-background sm:text-foreground50 sm:hover:text-foreground"
                             }
                         >
                             contact
