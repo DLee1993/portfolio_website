82e96b811b75248b52db5836aa9c44e6feda62a4
David Lee <Dai180293@gmail.com>
2024-11-05 03:31:50 +0000

    added portfolio data

---
+++ data/portfolioData.js
@@ -0,0 +1,63 @@
+export const projects = [
+    {
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae",
+        liveSite: "https://www.youtube.com",
+        techStack: ["next.js", "shadcn ui", "tailwind css"],
+    },
+    {
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae",
+        liveSite: "https://www.youtube.com",
+        techStack: ["next.js", "shadcn ui", "tailwind css"],
+    },
+];
+
+export const resume = [
+    {
+        title: "Education",
+        courses: [
+            {
+                courseProvider: "The Odin Project",
+                courseTitles: ["Foundations Course", "Full Stack JavaScript Course"],
+            },
+            {
+                courseProvider: "Udemy",
+                courseTitles: [
+                    "Mern Stack front to back",
+                    "React front to back",
+                    "The Web Developer Bootcamp",
+                    "The Modern Flexbox, Grid, Sass & Animation Developer",
+                    "GSAP animation library",
+                    "JavaScript Developer bootcamp",
+                    "Complete Node.js Developer",
+                ],
+            },
+        ],
+    },
+    {
+        title: "Technical Competence",
+        keynotes: [
+            "Proficient in the use of HTML, CSS, JavaScript",
+            "Moderate understanding of React",
+            "Experienced in using styling frameworks and UI Libraries ( Tailwind css, Shadcn ui, React Aria)",
+            "Familiar with open-source database management system MongoDB",
+            "Familiar with open-source Backend as a Service platform Appwrite",
+            "Advancing my experience with TypeScript and Next.js",
+        ],
+    },
+    {
+        title: "Personal Qualities",
+        keynotes: [
+            "Logical thinker with a keen eye for detail",
+            "Strong communicator, capable of articulating my thoughts in a concise manner ",
+            "Altruistic team player",
+            "Keen to stay ahead of the technological curve",
+            "Receptive to feedback in all aspects of my development",
+        ],
+    },
+    {
+        title: "Personal Interests",
+        keynotes: ["Kickboxing, I have found a sport that pushes me  beyond my limits"],
+    },
+];
