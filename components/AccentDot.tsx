5487ff4634435322f89c321b662fcfaebbf18f1d
David Lee <Dai180293@gmail.com>
2024-11-05 03:32:10 +0000

    changed dot from yellow to green

--- components/AccentDot.tsx
+++ components/AccentDot.tsx
@@ -1,8 +1,8 @@
 const AccentDot = () => {
     return (
-        <p className="relative flex size-[10px]">
-            <span className="motion-safe:animate-ping absolute inline-flex h-full w-full rounded-full bg-yellow-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full size-[10px] bg-yellow-500"></span>
+        <p className="relative flex size-[8px]">
+            <span className="motion-safe:animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full size-[8px] bg-green-500"></span>
         </p>
     );
 };
