6963d8dc187b8e3ea5325fa7c25facc5f3446c10
David Lee <Dai180293@gmail.com>
2024-11-06 10:58:04 +0000

    added all content and styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Header = () => {
-    return <header>header</header>;
+    const path = usePathname();
+    return (
+        <header className="customWidth h-20 flex justify-between items-center">
+            <Link href="/" className="font-medium">
+                dl.
+            </Link>
+            <ul className="flex justify-center items-center gap-2">
+                <li>
+                    <Link
+                        href="/info"
+                        className={`${
+                            path === "/info"
+                                ? "text-foreground"
+                                : "text-foreground50 hover:text-foreground"
+                        }`}
+                    >
+                        info
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="/contact"
+                        className={`${
+                            path === "/contact"
+                                ? "text-foreground"
+                                : "text-foreground50 hover:text-foreground"
+                        }`}
+                    >
+                        contact
+                    </Link>
+                </li>
+            </ul>
+        </header>
+    );
 };
 export default Header;
