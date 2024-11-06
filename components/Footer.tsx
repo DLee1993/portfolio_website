6cbec807d84d211e186ce3dee315734adfcb8c94
David Lee <Dai180293@gmail.com>
2024-11-06 11:02:50 +0000

    added all content and styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,11 @@
 const Footer = () => {
-    return <footer id="contactForm" className="">Footer</footer>;
+    return (
+        <footer className="customWidth flex flex-col min-[440px]:flex-row justify-between items-center border-t-[1px] border-foreground25 py-5 text-sm">
+            <p>Copyright 2024</p>
+            <p>
+                Designed & Developed by <span className="font-bold">David Lee</span>
+            </p>
+        </footer>
+    );
 };
 export default Footer;
