a88fa4f1204a2fd8716bb0f0b7d1bfdc313185f2
David Lee <Dai180293@gmail.com>
2024-11-07 13:32:58 +0000

    removed in place of available to work component

--- components/AccentDot.tsx
+++
@@ -1,9 +0,0 @@
-const AccentDot = () => {
-    return (
-        <p className="relative flex size-[8px]">
-            <span className="motion-safe:animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full size-[8px] bg-green-500"></span>
-        </p>
-    );
-};
-export default AccentDot;
