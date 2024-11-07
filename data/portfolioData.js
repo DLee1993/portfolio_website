d1e659377576054bfd7445d7def1e04b7673a646
David Lee <Dai180293@gmail.com>
2024-11-07 14:40:32 +0000

    added my technical skills data to the data array

--- data/portfolioData.js
+++ data/portfolioData.js
@@ -50,51 +50,37 @@ export const questions = [
     },
 ];
 
-export const resume = [
+export const technicalSkills = [
     {
-        title: "Education",
-        courses: [
-            {
-                courseProvider: "The Odin Project",
-                courseTitles: ["Foundations Course", "Full Stack JavaScript Course"],
-            },
-            {
-                courseProvider: "Udemy",
-                courseTitles: [
-                    "Mern Stack front to back",
-                    "React front to back",
-                    "The Web Developer Bootcamp",
-                    "The Modern Flexbox, Grid, Sass & Animation Developer",
-                    "GSAP animation library",
-                    "JavaScript Developer bootcamp",
-                    "Complete Node.js Developer",
-                ],
-            },
-        ],
+        skill: "HTML, CSS, JavaScript",
+        description: "Foundations",
     },
     {
-        title: "Technical Competence",
-        keynotes: [
-            "Proficient in the use of HTML, CSS, JavaScript",
-            "Moderate understanding of React",
-            "Experienced in using styling frameworks and UI Libraries ( Tailwind css, Shadcn ui, React Aria)",
-            "Familiar with open-source database management system MongoDB",
-            "Familiar with open-source Backend as a Service platform Appwrite",
-            "Advancing my experience with TypeScript and Next.js",
-        ],
+        skill: "Next.js",
+        description: "The React Framework for the Web",
     },
     {
-        title: "Personal Qualities",
-        keynotes: [
-            "Logical thinker with a keen eye for detail",
-            "Strong communicator, capable of articulating my thoughts in a concise manner ",
-            "Altruistic team player",
-            "Keen to stay ahead of the technological curve",
-            "Receptive to feedback in all aspects of my development",
-        ],
+        skill: "Typescript",
+        description: "JavaScript with syntax for types",
     },
     {
-        title: "Personal Interests",
-        keynotes: ["Kickboxing, I have found a sport that pushes me  beyond my limits"],
+        skill: "Tailwind css",
+        description: "Utility-first CSS framework",
+    },
+    {
+        skill: "Shadcn ui, React aria",
+        description: "Beautifully designed components",
+    },
+    {
+        skill: "GSAP, Framer motion",
+        description: "Animation libraries",
+    },
+    {
+        skill: "MongoDB",
+        description: "NoSQL Database",
+    },
+    {
+        skill: "Appwrite",
+        description: "Backend as a service",
     },
 ];
