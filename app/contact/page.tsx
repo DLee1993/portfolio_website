d7955ecc47bcf5e53a4897df46a1e2aa81eccfc9
David Lee <Dai180293@gmail.com>
2024-11-10 14:39:24 +0000

    added contact component to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,25 @@
-import Contact from "@/components/Contact";
 const page = () => {
-    return <Contact />;
+    return <h1>hello</h1>;
 };
 export default page;
+
+{
+    /**
+
+    <section className={path === "/contact" ? "customWidth" : ""}>
+            <aside className="flex justify-between items-center border-b border-foreground25">
+                <h2 className="text-subHeading font-semibold">Let&apos;s connect</h2>
+                <AvailableToWork />
+            </aside>
+            <p className="mt-10">
+                Thanks for checking out my portfolio. If you&apos;d like to get in touch, you can
+                reach me using the following methods.
+            </p>
+            {path === "/contact" && <ContactForm />}
+            <section className="w-fit my-12 flex justify-center items-center gap-2">
+                <SocialLinks />
+            </section>
+        </section>
+    
+*/
+}
