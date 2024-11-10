1e26c4712fb96e7fd8e8e5005720c2adfbde6ae3
David Lee <Dai180293@gmail.com>
2024-11-10 14:25:57 +0000

    changed font size to 14px

--- components/AvailableToWork.tsx
+++ components/AvailableToWork.tsx
@@ -1,6 +1,6 @@
 const AvailableToWork = () => {
     return (
-        <aside className="w-fit flex justify-center items-center gap-2 text-sm font-bold my-5 sm:my-7">
+        <aside className="w-fit flex justify-center items-center gap-2 text-xs font-bold">
             <p className="relative flex size-[8px]">
                 <span className="motion-safe:animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                 <span className="relative inline-flex rounded-full size-[8px] bg-green-500"></span>
